a690c0c254026f688c40d726987748339c912f9a
陈子安 <fengfeng1995@vip.qq.com>
2019-08-22 18:01:09 +0800

    修改查询,添加tag标签查询

--- db/blogConnection/blogConnection.js
+++ db/blogConnection/blogConnection.js
@@ -26,7 +26,7 @@ module.exports={
       var param = req.body;
       var _res = res;
       var data = {};
-      connection.query(`select count(*) from article where isDelete=0`, (err, result)=> {
+      connection.query(`select count(*) from article where isDelete=0  ${param.categoryId?`and  categoryId = ${param.categoryId}`:''} ${param.keyword?`and  articleTitle like '%${param.keyword}%'`:''} ${param.tagId?`and find_in_set(${param.tagId}, tagIds)`:''}` , (err, result)=> {
         if (!result) {
           data.result = {
             responseCode: '5000',
@@ -43,12 +43,16 @@ module.exports={
           }
           let total =  data.result.data.total 
           let totalPage =  total % param.pageSize == 0 ? total / param.pageSize : Math.ceil(total / param.pageSize)
+          if (totalPage<1) {
+            totalPage = 1
+          }
           data.result.data.lastPage = totalPage
           if (param.page >= totalPage) {
             param.page = totalPage
             data.result.data.isLastPage = true
           }
-          connection.query(`select * from article ${param.categoryId?`where categoryId = ${param.categoryId}`:''} ${param.keyword?`where articleTitle like '%${param.keyword}%'`:''}  order by createdTime limit ${param.pageSize} offset ${(param.page-1)*param.pageSize}`,(err,result) => {
+          console.log(param,totalPage)
+          connection.query(`select * from article ${param.categoryId?`where categoryId = ${param.categoryId}`:''} ${param.keyword?`where articleTitle like '%${param.keyword}%'`:''} ${param.tagId?`where find_in_set(${param.tagId}, tagIds)`:''} order by createdTime limit ${param.pageSize} offset ${(param.page-1)*param.pageSize}`,(err,result) => {
             if(!result) {
               data.result = {
                 responseCode: '5000',
